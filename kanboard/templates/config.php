f88f730747a078e30508bd82e02ac893d94cba56
Malte Krupa <github-nqyroppg@nafn.de>
2024-07-27 15:27:19 +0200

    Add linting

--- kanboard/templates/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_RUN_MIGRATIONS', true);
-define('DB_DRIVER', 'postgres');
-define('DB_USERNAME', '{{ kanboard_psql_username }}');
-define('DB_PASSWORD', '{{ kanboard_psql_password }}');
-define('DB_HOSTNAME', '{{ kanboard_psql_host }}');
-define('DB_NAME', '{{ kanboard_psql_database }}');
-define('DB_PORT', 5432);
-define('DB_TIMEOUT', null);
